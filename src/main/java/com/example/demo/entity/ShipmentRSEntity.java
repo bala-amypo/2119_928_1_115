749edbe47ecb1928d24d224a2c4483e058607f3c
bala-amypo <bala@amypo.in>
2025-12-18 09:23:45 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/ShipmentRSEntity.java
@@ -0,0 +1,80 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "shipments", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "shipmentCode")
+})
+public class ShipmentRSEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String shipmentCode;
+    private String origin;
+    private String destination;
+    private String productType;
+    private String status;
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.status = "IN_TRANSIT";
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getShipmentCode() {
+        return shipmentCode;
+    }
+
+    public void setShipmentCode(String shipmentCode) {
+        this.shipmentCode = shipmentCode;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
