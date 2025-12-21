24e0c367eeaa7064a1079af6a895ea7ddb65f6d0
bala-amypo <bala@amypo.in>
2025-12-21 11:18:53 +0000

    User commit

--- src/main/java/com/example/demo/repository/BreachRRepository.java
+++ src/main/java/com/example/demo/repository/BreachRRepository.java
@@ -1,12 +1,5 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.BreachDSEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
 public interface BreachRRepository
         extends JpaRepository<BreachDSEntity, Long> {
 
-    List<BreachDSEntity> findByShipmentId(Long shipmentId);
+    List<BreachDSEntity> findByShipment_Id(Long shipmentId);
 }
