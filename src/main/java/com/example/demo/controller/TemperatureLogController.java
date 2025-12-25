055422f62e6fc4bb47a2dc3619e5569f2e5b2519
bala-amypo <bala@amypo.in>
2025-12-25 16:49:45 +0000

    User commit

--- src/main/java/com/example/demo/controller/TemperatureLogController.java
+++ src/main/java/com/example/demo/controller/TemperatureLogController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@@RestController
+@RestController
 @RequestMapping("/api/logs")
 @Tag(name = "Temperature Logs")
 public class TemperatureLogController {
