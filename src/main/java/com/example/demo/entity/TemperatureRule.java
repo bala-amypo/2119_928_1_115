9537f84a65901e9934868d97b5ec6f80e8d7afd2
bala-amypo <bala@amypo.in>
2025-12-25 09:25:23 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/TemperatureRule.java
@@ -0,0 +1,44 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "temperature_rules")
+public class TemperatureRule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productType;
+    private Double minTemp;
+    private Double maxTemp;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public Double getMinTemp() {
+        return minTemp;
+    }
+
+    public void setMinTemp(Double minTemp) {
+        this.minTemp = minTemp;
+    }
+
+    public Double getMaxTemp() {
+        return maxTemp;
+    }
+
+    public void setMaxTemp(Double maxTemp) {
+        this.maxTemp = maxTemp;
+    }
+}
