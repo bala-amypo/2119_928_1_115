1c302dbe7542e8b9406ebda98cec9986029afbbc
bala-amypo <bala@amypo.in>
2025-12-25 10:16:23 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserSCTL.java
+++ src/main/java/com/example/demo/controller/UserSCTL.java
@@ -2,21 +2,39 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
-
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
-public class UserSCTL {
+public class UserController {
 
     private final UserService userService;
 
-    public UserSCTL(UserService userService) {
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
+  
     @GetMapping("/{id}")
     public User getUserById(@PathVariable Long id) {
         return userService.getUserById(id);
+    }
+
+    /**
+     * Register new user
+     * URL: POST /api/users
+     */
+    @PostMapping
+    public User registerUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+    /**
+     * Get user by email
+     * URL: GET /api/users/email/{email}
+     */
+    @GetMapping("/email/{email}")
+    public User getUserByEmail(@PathVariable String email) {
+        return userService.findByEmail(email);
     }
 }
